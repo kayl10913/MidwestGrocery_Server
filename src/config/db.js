e43bd7d60095f64cd2266e8134207f05093ee326
kayl10913 <calingasankylematthew@gmail.com>
2025-10-16 19:26:45 +0800

    dbdbdb

--- src/config/db.js
+++ src/config/db.js
@@ -4,8 +4,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Optional SSL for providers like Hostinger (set DB_SSL=true in env when required)
+// Some providers require SNI (TLS servername) to match the cert hostname.
+// If your DB_HOST is an IP, set DB_SSL_SERVERNAME to the host name from your provider.
 const sslOption = process.env.DB_SSL === 'true'
-  ? { rejectUnauthorized: false }
+  ? {
+      rejectUnauthorized: false,
+      ...(process.env.DB_SSL_SERVERNAME ? { servername: process.env.DB_SSL_SERVERNAME } : {}),
+      minVersion: 'TLSv1.2',
+    }
   : undefined;
 
 const pool = mysql.createPool({
